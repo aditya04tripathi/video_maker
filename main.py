7ed1be0bedf7e0a932b9427c0b8bed4cf38ba87d
Aditya Tripathi <atri0048@student.monash.edu>
2025-04-03 21:28:10 +1100

    Refactor video paths to be relative and update caption formatting

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 username = os.getenv("IG_UNAME")
 password = os.getenv("IG_PASSW")
 
-day_count = 1
+base_dir = os.path.dirname(os.path.abspath(__file__))
 hashtags = [
     "#love", "#romance", "#couplegoals", "#relationshipgoals", "#inlove",
     "#foreverlove", "#soulmates", "#lovequotes", "#romantic", "#trueLove",
@@ -18,25 +18,20 @@ hashtags = [
     "#loveislove", "#happiness", "#cuteCouple", "#lovebirds", "#adorable",
     "#kisses", "#hugs", "#sweetheart", "#valentine", "#couplelife"
 ]
-template_path = "/Users/aditya/Desktop/video_maker/src/insta_template.mp4"
-output_path = "/Users/aditya/Desktop/video_maker/src/output_video.mp4"
+template_path = f"{base_dir}/src/insta_template.mp4"
+output_path = f"{base_dir}/src/output_video.mp4"
 date = datetime.now().strftime('%A, %B %d %Y')
-caption = f"""I love you so much, my love <3
+caption = f"""I love you so much, my love ❤️✨
 
-
-
-
-Account active since {day_count} days.
-Hashtags (ignore)
 {" " + " ".join(hashtags)}
 """
 add_text_to_video(
-    "/Users/aditya/Desktop/video_maker/src/insta_template.mp4",
-    "/Users/aditya/Desktop/video_maker/src/output_video.mp4",
+    template_path,
+    output_path,
     f"Remember <3\nIt's {date}\nYou are my everything\nI'm still falling in love with you\nand won't stop loving you, ilysm",
+    f"{base_dir}/src/Rubik-Regular.otf",
     50,
     "white"
 )
 
 upload_video_to_instagram(username, password, output_path, caption)
-day_count += 1
--- src/gen_video.py
+++ src/gen_video.py
@@ -1,13 +1,13 @@
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
 
-def add_text_to_video(input_video_path, output_video_path, text, font_size, color):
+def add_text_to_video(input_video_path, output_video_path, text, font, font_size, color):
     video = VideoFileClip(input_video_path)
 
     text_clip = TextClip(
         text,
         fontsize=font_size,
-        font="/Users/aditya/Desktop/video_maker/src/Rubik-Regular.otf",
+        font=font,
         color=color,
         method="caption",
         size=video.size,
@@ -16,4 +16,5 @@ def add_text_to_video(input_video_path, output_video_path, text, font_size, colo
     video_with_text = CompositeVideoClip([video, text_clip])
 
     video_with_text.write_videofile(
-        output_video_path, codec="libx264", audio_codec="aac")
+        output_video_path, codec="libx264", audio_codec="aac"
+    )
